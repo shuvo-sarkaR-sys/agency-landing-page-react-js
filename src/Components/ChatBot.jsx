608ffdcc7957429f2c69552e951578c48ed84691
shuvo-sarkaR-sys <129524615+shuvo-sarkaR-sys@users.noreply.github.com>
2025-11-11 18:52:09 +0000

    add

--- src/Components/ChatBot.jsx
+++ src/Components/ChatBot.jsx
@@ -14,7 +14,7 @@ const ChatBot = () => {
     const userMessage = { text: input, sender: "user" };
     setMessage([...messages, userMessage]);
     try {
-      const response = await fetch("http://localhost:5000/chatbot", {
+      const response = await fetch("https://chatbot-j11k.onrender.com/chatbot", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: input })
@@ -32,7 +32,7 @@ const ChatBot = () => {
 
   return (
     <div>
-      {Showbot ? (<div className='fixed left-3 bottom-8'>
+      {!Showbot ? (<div className='fixed left-3 bottom-8'>
         <button onClick={handleClick} className='relative text-white p-3 rounded-full bg-green-700'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-14">
           <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
         </svg>
